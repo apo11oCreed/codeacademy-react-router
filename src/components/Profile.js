df8759091a1ecb24b4d03ac6abee241fb4a9f083
nc <nelson.ncdesigns@gmail.com>
2022-05-06 11:27:45 -0400

    Redirects

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link, useRouteMatch } from "react-router-dom";
+import { Route, Link, useRouteMatch, Redirect } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
@@ -12,7 +12,11 @@ export default function Profile () {
   let {url,path}=useRouteMatch();
   
   // use loggedIn to return a Redirect
-  
+  if(loggedIn===false){
+    return (
+      <Redirect to='/sign-up' />
+    )
+  }
   return (
     <main>
       <h1>{currentUser.username}</h1>
